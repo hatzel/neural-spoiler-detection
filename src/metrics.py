cd42aae78260f174afb293fd68a677bf9a791354
Hans Ole Hatzel <hhatzel@gmail.com>
2019-10-03 21:44:51 +0200

    Simplify winpr calculation

--- src/metrics.py
+++ src/metrics.py
@@ -10,32 +10,31 @@ import math
 def winpr(reference_labels, computed_labels, window_size=3):
     if len(reference_labels) != len(computed_labels):
         raise Exception("Sequences need to be of equal length.")
+    items = {}
+    for item in reference_labels:
+        items[item] = None
+    for item in computed_labels:
+        items[item] = None
+    if len(items) > 2:
+        raise Exception("Segmentation data should only contain two labels")
+
     i = 1 - window_size
-    tps = []
-    tns = []
-    fps = []
-    fns = []
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
     while i < len(reference_labels):
         reference_window = reference_labels[max(0, i - 1):i + window_size + 1]
         computed_window = computed_labels[max(0, i - 1):i + window_size + 1]
         reference_boundaries = n_boundaries(reference_window)
         computed_boundaries = n_boundaries(computed_window)
-        tps.append(min(reference_boundaries, computed_boundaries))
+        tp += min(reference_boundaries, computed_boundaries)
         # The paper can be a bit confusing for this one
         # The window size is not equal to k but instead k = window_size + 1
-        tns.append(
-            len(reference_window) - 1 - max(reference_boundaries, computed_boundaries)
-        )
-        fps.append(max(0, computed_boundaries - reference_boundaries))
-        fns.append(max(0, reference_boundaries - computed_boundaries))
+        tn += len(reference_window) - 1 - max(reference_boundaries, computed_boundaries)
+        fp += max(0, computed_boundaries - reference_boundaries)
+        fn += max(0, reference_boundaries - computed_boundaries)
         i += 1
-    print(tns)
-    print(len(tns))
-    print(sum(tns))
-    tp = sum(tps)
-    tn = sum(tns)
-    fp = sum(fps)
-    fn = sum(fns)
     results = {
         "true_positives": tp,
         "true_negatives":  tn,
