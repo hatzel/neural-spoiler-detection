adc23ee3cd440479c00c94ee8e9bc7c51b89f614
Hans Ole Hatzel <hhatzel@gmail.com>
2019-10-10 11:07:39 +0200

    Adapt winpr to work with on batches of data

--- src/metrics.py
+++ src/metrics.py
@@ -5,39 +5,55 @@ Implemented as described in the 2012 paper by Martin Scaiano and Diana Inkpen.
 
 """
 import math
+import torch
 
 
-def winpr(reference_labels, computed_labels, window_size=3):
+def winpr(reference_labels, computed_labels, window_size=3, average="micro"):
+    if average != "micro":
+        raise Exception("Only the 'micro' averaging method is supported.")
     if len(reference_labels) != len(computed_labels):
         raise Exception("Sequences need to be of equal length.")
-    items = {}
-    for item in reference_labels:
-        items[item] = None
-    for item in computed_labels:
-        items[item] = None
-    if len(items) > 2:
-        raise Exception("Segmentation data should only contain two labels")
+    too_many_labels = False
+    if isinstance(reference_labels[0], torch.Tensor):
+        unique_labels = torch.unique(
+            torch.cat([*reference_labels, *computed_labels]).reshape(-1)
+        )
+        if len(unique_labels) > 2:
+            too_many_labels = True
+    else:
+        all_labels = set(*reference_labels) | set(*computed_labels)
+        print(all_labels)
+        if len(all_labels) > 2:
+            too_many_labels = True
+    if too_many_labels:
+        raise Exception(
+            "Segmentation data should only contain two labels"
+        )
 
-    i = 1 - window_size
     tp = 0
     tn = 0
     fp = 0
     fn = 0
-    while i < len(reference_labels):
-        reference_window = reference_labels[max(0, i - 1):i + window_size + 1]
-        computed_window = computed_labels[max(0, i - 1):i + window_size + 1]
-        reference_boundaries = n_boundaries(reference_window)
-        computed_boundaries = n_boundaries(computed_window)
-        tp += min(reference_boundaries, computed_boundaries)
-        # The paper can be a bit confusing for this one
-        # The window size is not equal to k but instead k = window_size + 1
-        tn += len(reference_window) - 1 - max(reference_boundaries, computed_boundaries)
-        fp += max(0, computed_boundaries - reference_boundaries)
-        fn += max(0, reference_boundaries - computed_boundaries)
-        i += 1
+
+    for reference_example, computed_example in zip(reference_labels, computed_labels):
+        if len(reference_example) != len(computed_example):
+            raise Exception("Inner sequences need to be of equal length.")
+        i = 1 - window_size
+        while i < len(computed_example):
+            reference_window = reference_example[max(0, i - 1):i + window_size + 1]
+            computed_window = computed_example[max(0, i - 1):i + window_size + 1]
+            reference_boundaries = n_boundaries(reference_window)
+            computed_boundaries = n_boundaries(computed_window)
+            tp += min(reference_boundaries, computed_boundaries)
+            # The paper can be a bit confusing for this one
+            # The window size is not equal to k but instead k = window_size + 1
+            tn += len(reference_window) - 1 - max(reference_boundaries, computed_boundaries)
+            fp += max(0, computed_boundaries - reference_boundaries)
+            fn += max(0, reference_boundaries - computed_boundaries)
+            i += 1
     results = {
         "true_positives": tp,
-        "true_negatives":  tn,
+        "true_negatives": tn,
         "false_positives": fp,
         "false_negatives": fn,
         "winP": tp / (tp + fp) if tp + fp > 0 else math.nan,
--- tests/test_winpr.py
+++ tests/test_winpr.py
@@ -1,10 +1,11 @@
 from src.metrics import winpr
 import math
+import torch
 
 
 def test_winpr_correct():
-    reference_labels = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-    computed_labels = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    reference_labels = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+    computed_labels = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
     result = winpr(reference_labels, computed_labels, window_size=3)
     assert result["true_positives"] == 4
     assert result["true_negatives"] == 40
@@ -15,8 +16,8 @@ def test_winpr_correct():
 
 
 def test_winpr_missed_boundary():
-    reference_labels = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-    computed_labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    reference_labels = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+    computed_labels = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
     result = winpr(reference_labels, computed_labels, window_size=3)
     assert result["true_positives"] == 0
     assert result["true_negatives"] == 40
@@ -27,13 +28,13 @@ def test_winpr_missed_boundary():
 
 
 def test_winpr_near_boundary():
-    reference_labels = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-    computed_labels = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    reference_labels = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+    computed_labels = [[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]]
     result = winpr(reference_labels, computed_labels, window_size=3)
     assert result["true_positives"] == 3
     # TODO: is this correct?
-    # It is the value from the original paper but 39 seems to make more sense to me.
-    assert result["true_negatives"] == 40
+    # 40 is the value from the original paper but 39 seems to make more sense to me.
+    assert result["true_negatives"] == 39
     assert result["false_positives"] == 1
     assert result["false_negatives"] == 1
     assert result["winP"] == 0.75
@@ -41,8 +42,8 @@ def test_winpr_near_boundary():
 
 
 def test_winpr_extra_boundary():
-    reference_labels = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-    computed_labels = [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    reference_labels = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+    computed_labels = [[1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
     result = winpr(reference_labels, computed_labels, window_size=3)
     assert result["true_positives"] == 4
     assert result["true_negatives"] == 36
@@ -53,8 +54,8 @@ def test_winpr_extra_boundary():
 
 
 def test_winpr_extra_boundaries():
-    reference_labels = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-    computed_labels = [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1]
+    reference_labels = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+    computed_labels = [[0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1]]
     result = winpr(reference_labels, computed_labels, window_size=3)
     assert result["true_positives"] == 4
     assert result["true_negatives"] == 32
@@ -62,3 +63,29 @@ def test_winpr_extra_boundaries():
     assert result["false_negatives"] == 0
     assert result["winP"] == 1/3
     assert result["winR"] == 1
+
+
+def test_winpr_tensors():
+    reference_labels = torch.tensor([[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]])
+    computed_labels = torch.tensor([[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]])
+    result = winpr(reference_labels, computed_labels, window_size=3)
+    assert result["true_positives"] == 4
+    assert result["true_negatives"] == 40
+    assert result["false_positives"] == 0
+    assert result["false_negatives"] == 0
+    assert result["winP"] == 1
+    assert result["winR"] == 1
+
+
+def test_winpr_batch_processing_tensors():
+    real = [
+        torch.tensor([1, 1, 1]),
+        torch.tensor([0, 0, 0]),
+    ]
+    computed = [
+        torch.tensor([1, 1, 1]),
+        torch.tensor([0, 0, 0]),
+    ]
+    result = winpr(real, computed)
+    assert result["false_positives"] == 0
+    assert result["false_negatives"] == 0
