9b717bdef8e1f0666c635cf6ab059b4b66d7abd7
Hans Ole Hatzel <hhatzel@gmail.com>
2019-10-18 09:31:01 +0200

    Improve visual alignment in confusion matrix

--- src/util.py
+++ src/util.py
@@ -11,5 +11,5 @@ def seed(seed):
 
 def print_confusion_matrix(confusion):
     print(f"{'':<18}{'Predicted Spoiler':>20}{'Predicted Non-Spoiler':>28}")
-    print(f"{'Actual Spoiler':<18}{confusion['tp']:>20}{confusion['fn']:>28}")
+    print(f"{'Actual Spoiler':>18}{confusion['tp']:>20}{confusion['fn']:>28}")
     print(f"{'Actual Non-Spoiler':<18}{confusion['fp']:>20}{confusion['tn']:>28}")
